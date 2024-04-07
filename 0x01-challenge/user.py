f7be2c9e6eec902947b989100a9f715c09a4cdbc
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-07 21:24:37 +0000

    Update user.py

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -3,12 +3,18 @@
 User class
 """
 
+
 class User():
     """ Documentation """
 
     def __init__(self):
         """ Documentation """
         self.__email = None
+
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
 
     @email.setter
     def email(self, value):
@@ -17,10 +23,7 @@ class User():
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
+
    
     
 if __name__ == "__main__":
