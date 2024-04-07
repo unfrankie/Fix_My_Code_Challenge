f8b2d264fef5fa2afb684df627e135895b69173e
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-07 21:27:42 +0000

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,11 +3,10 @@
 
 
 class square():
-    """ Square Class"""    
+    """ Square Class"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         """INIT"""
         for key, value in kwargs.items():
@@ -24,6 +23,7 @@ class square():
     def __str__(self):
         """STR"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
     """MAIN"""
